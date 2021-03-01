5ddb8be41570568fb9c7d14fba71c9603250b474
Simon Belbeoch <simon.belbeoch@octo.com>
2021-03-01 13:46:37 +0100

    [feat] add discount method

--- src/bookReader.js
+++ src/bookReader.js
@@ -1,5 +1,21 @@
+const DISCOUNT = {
+  0: () => 0,
+  1: () => 0,
+  2: price => price * 0.05,
+  3: price => price * 0.10,
+  4: price => price * 0.20,
+  5: price => price * 0.25,
+}
+
 const _sumPrice = (accumulator, book) => accumulator + book.price
+const _uniqueBooks = (value, index, books) => books.findIndex((book) => book.id === value.id) === index
+
 const totalPriceWithoutDiscount = (books) => books.reduce(_sumPrice, 0)
+const totalDiscountPrice = (books) => {
+  const eligibleBooks = books.filter(_uniqueBooks)
+  const discountMethod = DISCOUNT[eligibleBooks.length]
+  return parseFloat(discountMethod(eligibleBooks.reduce(_sumPrice, 0)).toFixed(2));
+}
 
 
-module.exports = { totalPriceWithoutDiscount }
+module.exports = { totalPriceWithoutDiscount, totalDiscountPrice }
--- src/bookReader.spec.js
+++ src/bookReader.spec.js
@@ -1,5 +1,5 @@
 const { books, FIRST, SECOND, THIRD, FOURTH, FIFTH } = require('./books')
-const { totalPriceWithoutDiscount } = require('./bookReader')
+const { totalPriceWithoutDiscount, totalDiscountPrice } = require('./bookReader')
 let usedBooks
 let expectedPrice
 
@@ -21,6 +21,88 @@ describe('bookReader', () => {
       })
       it(`returns the price of the two books (50 euros)`, () => {
         expect(totalPriceWithoutDiscount(usedBooks)).toBe(50)
+      })
+    })
+  })
+  describe('#totalDiscount', () => {
+    context('with one book', () => {
+      beforeEach(() => {
+        usedBooks = [{ ...FIRST}]
+        expectedPrice = FIRST.price
+      })
+      it(`returns 0`, () => {
+        expect(totalDiscountPrice(usedBooks)).toEqual(0)
+      })
+    })
+    context('with 2 differents books', () => {
+      beforeEach(() => {
+        usedBooks = [{ ...FIRST, price: 8 },{ ...SECOND, price: 8 }]
+        expectedPrice = 0.8
+      })
+      it(`returns the discount of the two books (${expectedPrice} euros)`, () => {
+        expect(totalDiscountPrice(usedBooks)).toBe(expectedPrice)
+      })
+      context('with one more common book', () => {
+        beforeEach(() => {
+          usedBooks = [...usedBooks, { ...FIRST, price: 8 }]
+        }),
+          it(`does not change the discount and returns the discount of the two books (${expectedPrice} euros)`, () => {
+            expect(totalDiscountPrice(usedBooks)).toBe(expectedPrice)
+          })
+      })
+    })
+    context('with 3 differents books', () => {
+      beforeEach(() => {
+        usedBooks = [{ ...FIRST, price: 8 },{ ...SECOND, price: 8 },{ ...THIRD, price: 8 }]
+        expectedPrice = 2.4
+      })
+      it(`returns the discount of the three books (${expectedPrice} euros)`, () => {
+        expect(totalDiscountPrice(usedBooks)).toBe(expectedPrice)
+      })
+
+      context('with one more common book', () => {
+        beforeEach(() => {
+          usedBooks = [...usedBooks, { ...FIRST, price: 8 }]
+        }),
+          it(`does not change the discount and returns the discount of the two books (${expectedPrice} euros)`, () => {
+            expect(totalDiscountPrice(usedBooks)).toBe(expectedPrice)
+          })
+      })
+    })
+    context('with 4 differents books', () => {
+      beforeEach(() => {
+        usedBooks = [{ ...FIRST, price: 8 },{ ...SECOND, price: 8 },{ ...THIRD, price: 8 },{ ...FOURTH, price: 8 }]
+        expectedPrice = 6.4
+      })
+      it(`returns the discount of the four books (${expectedPrice} euros)`, () => {
+        expect(totalDiscountPrice(usedBooks)).toBe(expectedPrice)
+      })
+
+      context('with one more common book', () => {
+        beforeEach(() => {
+          usedBooks = [...usedBooks, { ...FIRST, price: 8 }]
+        }),
+          it(`does not change the discount and returns the discount of the two books (${expectedPrice} euros)`, () => {
+            expect(totalDiscountPrice(usedBooks)).toBe(expectedPrice)
+          })
+      })
+    })
+    context('with 5 differents books', () => {
+      beforeEach(() => {
+        usedBooks = [{ ...FIRST, price: 8 },{ ...SECOND, price: 8 },{ ...THIRD, price: 8 },{ ...FOURTH, price: 8 },{ ...FIFTH, price: 8 }]
+        expectedPrice = 10
+      })
+      it(`returns the discount of the five books (${expectedPrice} euros)`, () => {
+        expect(totalDiscountPrice(usedBooks)).toBe(expectedPrice)
+      })
+
+      context('with one more common book', () => {
+        beforeEach(() => {
+          usedBooks = [...usedBooks, { ...FIRST, price: 8 }]
+        }),
+          it(`does not change the discount and returns the discount of the two books (${expectedPrice} euros)`, () => {
+            expect(totalDiscountPrice(usedBooks)).toBe(expectedPrice)
+          })
       })
     })
   })
